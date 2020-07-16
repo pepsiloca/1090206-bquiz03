88d922ecc09c8209f5a06d269e3eda06c36beec4
u8931011 <u8931011@gmail.com>
2020-07-16 09:58:07 +0800

    顯示所有的海報圖片

--- front/main.php
+++ front/main.php
@@ -26,6 +26,25 @@
   cursor: pointer;
 }
 
+.poster{
+  border :1px solid;
+  width: 200px;
+  height: 260px;
+  margin: 0 auto 20px auto;
+  position: relative;
+}
+
+.po{
+  width:100%;
+  height:100%;
+  position: absolute;
+  display: none;
+}
+
+.po img{
+  width:100%;
+}
+
 </style>
 
 <?php
@@ -37,9 +56,29 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
 <div class="half" style="vertical-align:top;">
       <h1>預告片介紹</h1>
       <div class="rb tab" style="width:95%;">
+      <!-- 海報區 -->
         <div class="poster">
 
+        <?php
+        
+          foreach($rows as $k=>$row){
+            echo "<div class='po'>";
+            echo "<img src='img/".$row['path']."'>";
+            echo "<div class='ct'>".$row['name']."</div>";
+            echo "</div>";
+
+          }
+        
+        
+        
+        
+        
+        
+        ?>
+
+
         </div>
+        <!-- 按鈕區 -->
         <div class="btns">
           <div class='control' onclick="shift('left')">&#9664;</div>
           <div class="nav">
@@ -60,7 +99,7 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
     </div>
 
       <script>
-
+        $(".po").eq(0).show(); //先顯示海報第一張
         let p=0;
         let total=$(".icon").length;   //陣列的長度有多少 算有幾張圖
         
