36ee24fea4a6da7878675749059b9d7c54713140
u8931011 <u8931011@gmail.com>
2020-07-16 16:09:36 +0800

    製作前端院線片清單功能

--- front/main.php
+++ front/main.php
@@ -16,41 +16,40 @@
 }
 .icon img{
   width:50px;
-  cursor: pointer;
+  cursor:pointer;
 }
 
 .control{
   width:45px;
   font-size:45px;
   text-align:center;
-  cursor: pointer;
+  cursor:pointer;
 }
-
 .poster{
-  border :1px solid;
-  width: 200px;
-  height: 260px;
-  margin: 0 auto 20px auto;
-  position: relative;
+    border: 1px solid;
+    width: 200px;
+    height: 260px;
+    margin: 0 auto 20px auto;
+    position:relative;
+    
 }
-
 .po{
-  width:100%;
-  height:100%;
-  background: white;
-  color:black;
-  position: absolute;
-  display: none;
+width:100%;
+height:100%;
+background:white;
+color:black;
+position:absolute;
+display:none;
 }
-
 .po img{
   width:100%;
 }
-
 </style>
 
 <?php
 $po=new DB("poster");
+
+//取出設定為顯示並排序過的資料
 $rows=$po->all(['sh'=>1]," order by `rank`");
 
 
@@ -58,29 +57,20 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
 <div class="half" style="vertical-align:top;">
       <h1>預告片介紹</h1>
       <div class="rb tab" style="width:95%;">
-      <!-- 海報區 -->
+      <!--海報區-->
         <div class="poster">
-
         <?php
-        
           foreach($rows as $k=>$row){
             echo "<div class='po' data-ani='".$row['ani']."'>";
             echo "<img src='img/".$row['path']."'>";
             echo "<div class='ct'>".$row['name']."</div>";
             echo "</div>";
-
           }
-        
-        
-        
-        
-        
-        
+
         ?>
-
-
         </div>
-        <!-- 按鈕區 -->
+
+        <!--按鈕區-->
         <div class="btns">
           <div class='control' onclick="shift('left')">&#9664;</div>
           <div class="nav">
@@ -93,30 +83,28 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
 
           ?>
           </div>
+          
           <div class='control' onclick="shift('right')">&#9654;</div>
 
 
         </div>
       </div>
     </div>
-
       <script>
-        $(".po").eq(0).show(); //先顯示海報第一張
+      $(".po").eq(0).show();
 
-        let auto=setInterval(slider, 3000);
-      
+      let auto=setInterval(slider, 3000);
 
-          function slider(){
-            let dom=$(".po:visible");
-            // let ani=$(dom).attr("data-ani");
-            let ani=$(dom).data('ani');
-            let next=$(dom).next();
-            if(next.length<=0){
-              next=$(".po").eq(0)
-            }
-
-            // console.log(next.length)
-            switch(ani){
+      function slider(){
+        let dom=$(".po:visible");
+        let ani=$(dom).data('ani');
+        let next=$(dom).next();
+        if(next.length<=0){
+          next=$(".po").eq(0)
+        }
+        
+        //console.log(next.length)
+        switch(ani){
           case 1:
             //淡入淡出
               $(dom).fadeOut(1000,function(){
@@ -140,112 +128,113 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
             $(dom).animate({width:0,height:0,left:100,top:130},function(){
                 $(next).css({width:0,height:0,left:100,top:130})
                 $(next).show();
-                $(next).animate({width:200,height:200,left:0,top:0})
+                $(next).animate({width:200,height:260,left:0,top:0})
                 $(dom).hide()
-                $(dom).css({width:200,height:200,left:0,top:0})
+                $(dom).css({width:200,height:260,left:0,top:0})
             })
 
         }
 
       }
-          $(".icon").on("click",function(){
-         
-            let index=$(".icon").index($(this))
-            $(".po").hide();
-            $(".po").eq(index).show();
 
-          })
-
-          $(".nav").hover(
-            function(){
-              clearInterval(auto)
-            },
-            function(){
-              auto=setInterval(slider, 3000);
-            }
-          )
-
-        let p=0;
-        let total=$(".icon").length;   //陣列的長度有多少 算有幾張圖
+      $(".icon").on("click",function(){
         
-        function shift(direct){
+        let index=$(".icon").index($(this))
+        $(".po").hide();
+        $(".po").eq(index).show();
+        
+      })
 
-          switch(direct){
-            case 'right':
-              if(p < (total-4)){    //每次只顯示4張
-                p++;
-                $(".icon").animate({right:80*p})
-              
-              }
-
-            break;
-            case 'left':
-              if(p>0){
-                p--;
-              $(".icon").animate({right:80*p})
-              }
-
-            break;
-          }
-
+      $(".nav").hover(
+        function(){
+          clearInterval(auto)
+        },
+        function(){
+          auto=setInterval(slider, 3000);
         }
 
+      )
+
+      let p=0;
+      let total=$(".icon").length;
+      function shift(direct){
+        switch(direct){
+          case 'right':
+            if(p<(total-4)){
+              p++;
+              $(".icon").animate({right:80*p});
+            }
+
+            break;
+          case 'left':
+              if(p>0){
+                p--;
+                $(".icon").animate({right:80*p})
+              }
+          break;
+        }
+      }
+      
+      
       </script>
-
-
-
+      
 <style>
-.mb{
-  width:48%;
-  height:160px;
-  margin:0.5%;
-  display: inline-block;
-}
-
+  .mb{
+    width:48%;
+    height:160px;
+    margin:0.5%;
+    display:inline-block;
+  }
 </style>
-
   <!---院線片-->  
     <div class="half">
       <h1>院線片清單</h1>
       <div class="rb tab" style="width:95%;">
-    <?php
-    $db=new DB("movie");
-    $today=date("Y-m-d");
-    $ondate=date("Y-m-d",strtotime("-2 days"));
+      <?php
+        
+        $db=new DB("movie");
+        $today=date("Y-m-d");
+        $ondate=date("Y-m-d",strtotime("-2 days"));
 
-    $total=$db->count(['sh'=>1], " && ondate >= '$ondate' && ondate <='$today'");
-    $div=4;
-    $now=(!empty($_GET['p']))?$_GET['p']:1;
-    $start=($now-1)*$div;
-    $rows=$db->all(['sh'=>1]," && ondate >= '$ondate' && ondate <='$today' order by rank limit $start,$div");
-    
-    foreach($rows as $row){
-
-    ?>
-
+        $total=$db->count(['sh'=>1]," && ondate >= '$ondate' && ondate <='$today'");
+        $div=4;
+        $pages=ceil($total/$div);
+        $now=(!empty($_GET['p']))?$_GET['p']:1;
+        $start=($now-1)*$div;
+        $rows=$db->all(['sh'=>1]," && ondate >= '$ondate' && ondate <='$today' order by rank limit $start,$div");
+        
+        foreach($rows as $row){
+      ?>
       <div class="mb">
-      <table>
-        <tr>
-          <td rowspan="3"><a href="?do=intro&id=<?=$row['id'];?>"></a><img src="img/<?=$row['poster'];?>" style="width:80px;height:100px;"></a></td>
-          <td><?=$row['name'];?></td>
-        </tr>
-        <tr>
-          <td><?=$row['level'];?></td>
-        </tr>
-        <tr>
-          <td><?=$row['ondate'];?></td>
-        </tr>
-      </table>
-
-
+          <table>
+            <tr>
+              <td rowspan="3"><a href="?do=intro&id=<?=$row['id'];?>"><img src="img/<?=$row['poster'];?>" style="width:80px;height:100px;"></a></td>
+              <td><?=$row['name'];?></td>
+            </tr>
+            <tr>
+              <td><img src="icon/<?=$row['level'];?>.png" style="width:15px"><?=$level[$row['level']];?></td>
+            </tr>
+            <tr>
+              <td><?=$row['ondate'];?></td>
+            </tr>
+          </table>
+          <div class="ct">
+            <button>劇情簡介</button><button>線上訂票</button>
+          </div>
 
       </div>
-   
-
       <?php
-      
       }
       ?>
-        <div class="ct"> </div>
+      <div class="ct">
+      <?php
+
+        for($i=1;$i<=$pages;$i++){
+            $font=($i==$now)?'24px':'18px';
+          echo "<a href='?p=$i' style='font-size:$font;text-decoration:none'> $i </a>";
+        }
+      
+      ?>
+    </div>
       </div>
     </div>
