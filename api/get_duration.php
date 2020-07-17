594b20a51a8f3982474e2a7d68e5b1e111532ef3
u8931011 <u8931011@gmail.com>
2020-07-17 14:25:34 +0800

    完成訂票選單之電影及日期選單

---
+++ api/get_duration.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "../base.php";
+
+$movie_id=$_Get['id'];
+$db=new DB("movie");
+$movie=$db->find($movie_id);
+
+$today=strtotime(date("Y-m-d"));
+$ondate=strtotime($movie['ondate']);
+
+for($i=0;$i<3;$i++){
+    $chk=strtotime("+$i days",$ondate);
+    if($chk>=$today){
+        echo "<option value='".date("Y-m-d",$chk)."'>".date("m月d日l",$chk)."</option>";
+    }
+}
+
+?>
